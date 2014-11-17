e060750446aeb76118b1d9c34e1bce3750f369a6
Janus Hasseriis <janus@hasseriis.net>
2014-11-17 21:48:25 +0100

    Capistrano test

---
+++ app/controllers/teams_controller.rb
@@ -0,0 +1,44 @@
+class TeamsController < ApplicationController
+	def index
+		@teams = Team.all
+	end	
+	
+	def show
+		# @team = Team.find(params[:id])
+	end	
+	
+	def new
+		
+	end
+	
+	def create
+		@team = Team.new(team_params)
+		
+		respond_to do |format|
+			if @team.valid?
+				if @team.save
+					@team.subscribe
+					flash.now[:notice] = 'Team was created'
+					format.html { redirect_to @team, notice: 'Team was succesfully created.'}
+					format.js { }
+					#format.json {
+					#	render json: @team, status: :created, location: @team 
+					#}
+				else
+					format.html { render action: 'new'}
+					#format.json { render json: @team.errors, status: :unprocesable_entity}
+				end
+			else
+				flash[:notice] = 'Please make sure you\'ve filled out all inputs and typed a working email'
+				format.html { render action: 'new'}
+				format.js { render action: 'error'}
+				#format.json { render json: @team.errors, status: :unprocesable_entity}				
+			end
+		end
+	end
+	
+	private
+		def team_params
+			params.require(:team).permit(:nick, :email, :game)
+		end
+end
---
+++ app/models/team.rb
@@ -0,0 +1,24 @@
+class Team < ActiveRecord::Base
+
+	#column :nick, :string
+	#column :email, :string
+	#column :games, :text
+
+	validates_presence_of :nick
+	validates_presence_of :email 
+	validates_presence_of :game 
+	validates_format_of :email, 
+		:with => /\A[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}\z/i	
+
+	def subscribe
+		mailchimp = Gibbon::API.new(Rails.application.secrets.mailchimp_api_key)
+		result = mailchimp.lists.subscribe({
+			:id => Rails.application.secrets.mailchimp_list_id,
+			:email => {:email => self.email},
+			:double_optin => false,
+			:update_existing => true,
+			:send_welcome => true
+			})
+		Rails.logger.info("Subscribed #{self.email} to MailChimp") if result
+	end
+end
